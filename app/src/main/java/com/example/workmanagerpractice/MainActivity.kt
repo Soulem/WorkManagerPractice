bf1158417c1ef86dc62f00de9e97079e449a7ad8
Soulem <ewilcher@fullsail.edu>
2021-07-21 21:36:14 -0400

    Initial commit
    Added comments to help people follow the code

--- app/src/main/java/com/example/workmanagerpractice/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerpractice/MainActivity.kt
@@ -13,14 +13,26 @@ class MainActivity() : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val myView : TextView = findViewById(R.id.string_textView)
         myView.text = "0"
+        // Work Manager has a hard time doing work for the activity.
+        // Will need to send it a bundle
+        /*val input: Bundle = Bundle().apply {
+            putString("some_key", "some_value")
+        }*/
 
+        // The worker and request
+        // using PeriodWorkRequestBuilder<T> will give us a WorkRequest.
+        // This is what we are going to use to give to the work manager.
+        // The minimum interval is 15 minutes.  if you're testing this I hope
+        // you have made time.
         val uploadWorkRequest =
             PeriodicWorkRequestBuilder<UploadWorker>(15, TimeUnit.MINUTES)
-                .setInitialDelay(5, TimeUnit.SECONDS)
                 .build()
 
+        // Queue it up in the work manager.
+        // The WorkManager is a singleton that needs a context.
         WorkManager
             .getInstance(this)
             .enqueueUniquePeriodicWork("My Work", ExistingPeriodicWorkPolicy.KEEP, uploadWorkRequest)
--- app/src/main/java/com/example/workmanagerpractice/UploadWorker.kt
+++ app/src/main/java/com/example/workmanagerpractice/UploadWorker.kt
@@ -2,19 +2,22 @@ package com.example.workmanagerpractice
 
 import android.content.Context
 import android.util.Log
-import android.widget.TextView
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
+// Your worker class needs to be a class that takes in a Context and a WorkerParameters.
+// It must implement/derive (not sure yet) from Worker.
 class UploadWorker(appContext: Context, workerParams: WorkerParameters):
     Worker(appContext, workerParams) {
 
+    // The only method you need to override is "doWork" which returns a Result (So many of these!  THIS IS NOT THE RESULT FROM THE API!)
     override fun doWork(): Result {
 
-        // Do the work here--in this case, upload the images.
+        // Do the work here.
         Log.d("TAG_X", "HELLO FROM WORKMANAGER")
 
-        // Indicate whether the work finished successfully with the Result
+        // Indicate whether the work finished successfully with the Result.
+        // This is handled by the developer.
         return Result.success()
     }
 }
